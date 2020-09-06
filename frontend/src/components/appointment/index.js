6c9cedf061de6d3553af4fa10246c59fb99b08f7
Nicholas Siu <ncyk.siu@gmail.com>
2020-09-05 23:02:45 -0700

    resizing of window changes calendar size

--- frontend/src/components/appointment/index.js
+++ frontend/src/components/appointment/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import AppointmentWrapperSC from './appointment-wrapper-sc'
 
-const Appointment = ({appointment}) => {
-  const {appointment_name} = appointment
+const Appointment = ({appointment, screenWidth}) => {
+  const {appointment_name, start_date} = appointment
+  const timeFormat = {
+    hour: '2-digit', 
+    minute: '2-digit',
+    hour12: true
+  }
+
+  const startTime = new Date(start_date).toLocaleTimeString([], timeFormat)
+  const appointmentWidth = screenWidth > 250 ? 250 - 16 : screenWidth - 18
 
   return (
-    <AppointmentWrapperSC>
-      {appointment_name}
+    <AppointmentWrapperSC screenWidth={appointmentWidth}>
+      {`${startTime} ${appointment_name}`}
     </AppointmentWrapperSC>
   )
 }
--- frontend/src/components/day-of-week/index.js
+++ frontend/src/components/day-of-week/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import DayOfWeekWrapperSC from './day-of-week-sc'
 
 const WeekDay = ({weekDay}) => {
+  const [windowSize, setWindowSize] = useState({width: undefined, height: undefined})
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const screenWidth = windowSize.width/7
 
   return (
-    <DayOfWeekWrapperSC weekDay={weekDay}>
+    <DayOfWeekWrapperSC weekDay={weekDay} screenWidth={screenWidth}>
         <div>
           <p>
           {weekDay}
