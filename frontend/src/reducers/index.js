db32f41c703acac6ebdf709c6963dea88a2e8a1e
Nicholas Siu <ncyk.siu@gmail.com>
2020-09-29 15:13:23 -0700

    add error and auth reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux'
 import calendarInfoReducer from './calendarInfoReducer'
 import appointmentReducer from './appointmentReducer'
+import errorReducer from './errorReducer'
+import authReducer from './authReducer'
 
 export default combineReducers({
   calendarInfo: calendarInfoReducer,
-  appointment: appointmentReducer
+  appointment: appointmentReducer,
+  error: errorReducer,
+  auth: authReducer
 })
