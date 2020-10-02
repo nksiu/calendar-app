b486748ef53a24a937abb786b975c70289a042ee
Nicholas Siu <ncyk.siu@gmail.com>
2020-10-02 00:06:42 -0700

    introduce env vars

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
-const config = require('config')
 const jwt = require('jsonwebtoken')
 const auth = require('../../middleware/auth')
 
@@ -28,7 +27,7 @@ router.post('/', (req, res) => {
       
       jwt.sign(
         {id: user.id},
-        config.get('jwtSecret'),
+        process.env.JWT_SECRET,
         {expiresIn: 3600},
         (err, token) => {
           if (err) throw err
